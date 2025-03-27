d125b0e063dcdc29f3685db6bf07c40882273c84
Luis-Moretti <ma4muh@gmail.com>
2025-03-28 00:18:13 +0300

    Update restart.py

--- restart.py
+++ restart.py
@@ -1,9 +1,9 @@
 import psutil, time, os
 
 while (True):
-    print('123')
     for pid in psutil.pids():
         p = psutil.Process(pid)
+        print(p.name().count('python'))
         if p.name().count('python') > 0:
             print(p.cmdline())
             if len(p.cmdline()) > 1 and not "main.py" in p.cmdline()[1]:
