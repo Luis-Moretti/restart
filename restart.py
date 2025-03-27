5aed5b259f1292ffc4113169958f95e998067a39
Luis-Moretti <ma4muh@gmail.com>
2025-03-28 00:19:14 +0300

    Update restart.py

--- restart.py
+++ restart.py
@@ -3,12 +3,10 @@ import psutil, time, os
 while (True):
     for pid in psutil.pids():
         p = psutil.Process(pid)
-        print(p.name().count('python'))
-        if p.name().count('python') > 0:
-            print(p.cmdline())
-            if len(p.cmdline()) > 1 and not "main.py" in p.cmdline()[1]:
-               print("Restarting")
-               os.chdir('..')
-               os.chdir('./assecc')
-               os.system('python main.py')
+        print(p.cmdline())
+        if len(p.cmdline()) > 1 and not "main.py" in p.cmdline()[1]:
+            print("Restarting")
+            os.chdir('..')
+            os.chdir('./assecc')
+            os.system('python main.py')
     time.sleep(5)
