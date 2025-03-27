14c6b7cb56657fd21ee487aa8a027ec96321cbe7
Luis-Moretti <ma4muh@gmail.com>
2025-03-28 00:14:23 +0300

    Update restart.py

--- restart.py
+++ restart.py
@@ -5,6 +5,7 @@ while(True):
         p = psutil.Process(pid)
         if p.name().count('python') > 0:
             if len(p.cmdline()) > 1 and not "main.py" in p.cmdline()[1]:
+               print("Restarting")
                os.chdir('..')
                os.chdir('./assecc')
                os.system('python main.py')
