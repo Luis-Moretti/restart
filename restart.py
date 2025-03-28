4f8a18df734bee3a30802470d89c669aecebd6b8
Luis-Moretti <ma4muh@gmail.com>
2025-03-28 22:05:32 +0300

    Update restart.py

--- restart.py
+++ restart.py
@@ -5,9 +5,9 @@ while (True):
         for pid in psutil.pids():
             p = psutil.Process(pid)
             if len(p.cmdline()) > 1 and not "main.py" in p.cmdline()[1]:
-                print("Restarting")
+                print("Restart")
                 os.chdir('/data/data/com.termux/files/home/assecc')
                 os.system('python main.py')
-        time.sleep(15)
+            time.sleep(30)
     except:
         pass
