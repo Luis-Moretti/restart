e3d3788b77adcb760fabba5d5786d4aa8eaa6bfc
Luis-Moretti <ma4muh@gmail.com>
2025-04-01 15:12:38 +0300

    Update restart.py

--- restart.py
+++ restart.py
@@ -4,13 +4,13 @@ while (True):
     try:
         for pid in psutil.pids():
             p = psutil.Process(pid)
-            if len(p.cmdline()) > 1 and not "unmain.py" in p.cmdline()[1]:
-                os.chdir('/data/data/com.termux/files/home/assecc')
-                os.system('python unmain.py')
             if len(p.cmdline()) > 1 and not "main.py" in p.cmdline()[1]:
                 print("Restart")
                 os.chdir('/data/data/com.termux/files/home/assecc')
                 os.system('python main.py')
+            if len(p.cmdline()) > 1 and not "unmain.py" in p.cmdline()[1]:
+                os.chdir('/data/data/com.termux/files/home/assecc')
+                os.system('python unmain.py')
         time.sleep(5)
     except:
         print('Краш1')
