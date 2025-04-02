7c6ee4d8a97ca63a1280117a4a5d9c6281439ddd
Luis-Moretti <ma4muh@gmail.com>
2025-04-02 23:03:21 +0300

    Update restart.py

--- restart.py
+++ restart.py
@@ -8,9 +8,6 @@ while (True):
                 print("Restart")
                 os.chdir('/data/data/com.termux/files/home/assecc')
                 os.system('python main.py')
-            if len(p.cmdline()) > 1 and not "unmain.py" in p.cmdline()[1]:
-                os.chdir('/data/data/com.termux/files/home/assecc')
-                os.system('python unmain.py')
         time.sleep(5)
     except:
         print('Краш1')
