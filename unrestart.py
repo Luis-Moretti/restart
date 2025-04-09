f3191b7970afc2d2fe65d2495342dbc618385a6f
Luis-Moretti <ma4muh@gmail.com>
2025-04-09 19:45:57 +0300

    Create unrestart.py

---
+++ unrestart.py
@@ -0,0 +1,13 @@
+import psutil, time, os
+
+while (True):
+    try:
+        for pid in psutil.pids():
+            p = psutil.Process(pid)
+            if len(p.cmdline()) > 1 and not "unmain.py" in p.cmdline()[1]:
+                print("Restart")
+                os.chdir('/data/data/com.termux/files/home/assecc')
+                os.system('python unmain.py')
+        time.sleep(5)
+    except:
+        print('Краш1')
